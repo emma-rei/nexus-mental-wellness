ca7abb3b0ee0dd17a42813a7b824ae2a6f9c8f7e
emma-rei <122436109+emma-rei@users.noreply.github.com>
2023-07-31 16:14:18 +0800

    add snake game
    SnakeGameApp.start() to launch snake game.
    TODO: implement back button to go back to home.

---
+++ games/snake/SnakeGameApp.java
@@ -0,0 +1,16 @@
+package org.nexus.games.snake;
+
+public class SnakeGameApp {
+    public void start() {
+        SnakeGameForm form = new SnakeGameForm();
+        form.show();
+    }
+
+    public void stop() {
+        // Perform any cleanup tasks here
+    }
+
+    public void destroy() {
+        // Perform any cleanup tasks here
+    }
+}
